815841db78ccf8f5333df0cbe9cc992fd7b9308b
Artsiom Shablinski <artsiomshablinskiy@gmail.com>
2022-05-26 10:09:41 +0200

    final

--- src/utils/Test.java
+++ src/utils/Test.java
@@ -12,7 +12,17 @@ public class Test {
         double r = 0.15; // lower workload boundary
         int z = 3; // number of tries in 1st algoritm
         int N = 10; // number of CPU's in ds
-        int processes = 10000;
+        int processes = 1000;
+
+
+        System.out.println("_____________________________________________");
+        System.out.println("Parameters:");
+        System.out.println("p:" + p);
+        System.out.println("r:" + r);
+        System.out.println("z:" + z);
+        System.out.println("N:" + N);
+        System.out.println("number of processes:"+ processes);
+
 
         DistributedSystem ds = new DistributedSystem(p,r,z,N,processes, new FirstStrategy());
         ds.run();
